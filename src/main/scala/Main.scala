ff8ff76c86ee8229fdc8cb2f1a9db887fce0025f
tgodzik <tomek.godzik@gmail.com>
2019-10-22 13:44:44 +0200

    Add more examples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,13 @@ import scala.concurrent.Await
 
 object Main {
 
-  """|
-     |     | Content 
-     |     | with pipes
-     |
-     |""".stripMargin
+  def main(args: Array[String]) {
 
-  override def equals(obj: Any): Boolean = ???
+    val name: Option[String] = Some("Anne")
+
+    name match {
+      case None    => println("Hello <unknown>")
+      case Some(x) => println(s"Hello $x")
+    }
+  }
 }
--- src/test/scala/ZigZagTest.scala
+++ src/test/scala/ZigZagTest.scala
@@ -1,4 +1,9 @@
 class ZigZagTest extends org.scalatest.FunSuite {
   test("zigzag") {
+    assert(1 == 1)
+  }
+
+  test("zigzag-fail") {
+    assert(1 == 2)
   }
 }
