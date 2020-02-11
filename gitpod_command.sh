f37f89ef16a420ac4e4a01d5c3dbf687c42d057b
tgodzik <tomek.godzik@gmail.com>
2020-02-11 21:20:10 +0100

    Move some things from prebuild

--- gitpod_command.sh
+++ gitpod_command.sh
@@ -1,9 +1,13 @@
  #!/bin/bash
 
 export PATH=$PATH:/usr/local/openjdk-8/bin
-alias bloop=~/.bloop/bloop
+
+curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py | python
+curl -L https://piccolo.link/sbt-1.3.8.tgz > ~/sbt.tar.gz
+tar -C ~ -xvf ~/sbt.tar.gz
+echo "alias bloop=~/.bloop/bloop" >> ~/.bashrc
+source ~/.bloop/bash/bloop
 alias sbt=~/sbt/bin/sbt
-if [ -f "~/.bloop/bash/bloop" ]; then
-    source ~/.bloop/bash/bloop
-    bloop server &>/dev/null &
-fi
+alias bloop=~/.bloop/bloop
+bloop server &>/dev/null &
+
