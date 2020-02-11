71ab01977e319e7cdf48c3965f33f193d8f4c3e7
tgodzik <tomek.godzik@gmail.com>
2020-02-11 21:13:16 +0100

    Last touches

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export PATH=$PATH:/usr/local/openjdk-8/bin
-curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py | python &&
-echo "alias bloop=~/.bloop/bloop" >> ~/.bashrc &&
-curl -L https://piccolo.link/sbt-1.3.8.tgz > ~/sbt.tar.gz &&
-tar -C ~ -xvf ~/sbt.tar.gz &&
-~/.bloop/bloop server &>/dev/null &
+curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py | python
+echo "alias bloop=~/.bloop/bloop" >> ~/.bashrc
+curl -L https://piccolo.link/sbt-1.3.8.tgz > ~/sbt.tar.gz
+tar -C ~ -xvf ~/sbt.tar.gz
+source ./gitpod_command.sh
 ~/sbt/bin/sbt -Dbloop.export-jar-classifiers=sources bloopInstall
