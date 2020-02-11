d1677fd65bb0cc7f0c203bd2e6180336bdd8b436
Tomasz Godzik <tgodzik@users.noreply.github.com>
2020-02-11 17:16:03 +0100

    Update setup_bloop.sh

--- setup_bloop.sh
+++ setup_bloop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -L https://github.com/scalacenter/bloop/releases/download/v1.3.4/install.py | python
+curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py | python
 echo "alias bloop=~/.bloop/bloop" >> ~/.bashrc
 alias bloop=~/.bloop/bloop
 ~/.bloop/bloop server &>/dev/null &
