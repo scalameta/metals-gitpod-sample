0ba126f4bcf2b2d5273907f6385ba4ef7b515d8c
tgodzik <tomek.godzik@gmail.com>
2020-04-02 17:00:45 +0200

    Use GITPOD_REPO_ROOT in most places

--- gitpod_command.sh
+++ gitpod_command.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
-source ./.metals/bloop_local/bash/bloop
-alias sbt=./.metals/sbt/bin/sbt
-alias bloop=./.metals/bloop_local/bloop
+source $GITPOD_REPO_ROOT/.metals/bloop_local/bash/bloop
+alias sbt=$GITPOD_REPO_ROOT/.metals/sbt/bin/sbt
+alias bloop=$GITPOD_REPO_ROOT/.metals/bloop_local/bloop
 bloop server &>/dev/null &
--- gitpod_init.sh
+++ gitpod_init.sh
@@ -4,27 +4,26 @@ CURRENT_DIR="${pwd}"
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
 curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py > ~/bloop_install.py
-python ~/bloop_install.py --dest ./.metals/bloop_local
+python ~/bloop_install.py --dest $GITPOD_REPO_ROOT/.metals/bloop_local
 curl -L https://piccolo.link/sbt-1.3.9.tgz > ~/sbt.tar.gz
-mkdir -p ./.metals/sbt
-tar -C ~ -xvf ~/sbt.tar.gz -C $(pwd)/.metals
-echo "alias bloop=./.metals/bloop_local/bloop" >> ~/.bashrc
+mkdir -p $GITPOD_REPO_ROOT/.metals/sbt
+tar -C ~ -xvf ~/sbt.tar.gz -C $GITPOD_REPO_ROOT/.metals
 
-echo "-Dsbt.coursier.home=./.metals/coursier" >> .jvmopts
+echo "-Dsbt.coursier.home=$GITPOD_REPO_ROOT/.metals/coursier" >> .jvmopts
 echo "-Dcoursier.cache=$GITPOD_REPO_ROOT/.metals/coursier" >> .jvmopts
-echo "-sbt-dir ./.metals/sbt" >> .sbtopts
-echo "-sbt-boot ./.metals/sbt/boot" >> .sbtopts
-echo "-ivy ./.metals/.ivy2" >> .sbtopts
-
-source ./.metals/bloop_local/bash/bloop
-alias sbt=./.metals/sbt/bin/sbt
-alias bloop=./.metals/bloop_local/bloop
-bloop server &>/dev/null &
-sbt -Dbloop.export-jar-classifiers=sources bloopInstall
-bloop compile --cascade root
+echo "-sbt-dir $GITPOD_REPO_ROOT/.metals/sbt" >> .sbtopts
+echo "-sbt-boot $GITPOD_REPO_ROOT/.metals/sbt/boot" >> .sbtopts
+echo "-ivy $GITPOD_REPO_ROOT/.metals/.ivy2" >> .sbtopts
 
 # TODO should use the Metals  
 METALS_VERSION=0.8.3
-curl -Lo ./.metals/cs https://git.io/coursier-cli-linux && chmod +x ./.metals/cs
-./.metals/cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=./.metals/coursier 
-./.metals/cs fetch org.scalameta:scalafmt-cli_2.12:2.4.2 --cache=./.metals/coursier 
+curl -Lo $GITPOD_REPO_ROOT/.metals/cs https://git.io/coursier-cli-linux && chmod +x $GITPOD_REPO_ROOT/.metals/cs
+$GITPOD_REPO_ROOT/.metals/cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=$GITPOD_REPO_ROOT/.metals/coursier 
+$GITPOD_REPO_ROOT/.metals/cs fetch org.scalameta:scalafmt-cli_2.12:2.4.2 --cache=$GITPOD_REPO_ROOT/.metals/coursier 
+
+source $GITPOD_REPO_ROOT/.metals/bloop_local/bash/bloop
+alias sbt=$GITPOD_REPO_ROOT/.metals/sbt/bin/sbt
+alias bloop=$GITPOD_REPO_ROOT/.metals/bloop_local/bloop
+bloop server &>/dev/null &
+sbt -Dbloop.export-jar-classifiers=sources bloopInstall
+bloop compile --cascade root
