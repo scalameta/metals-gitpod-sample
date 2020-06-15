3a997e066f2113958a6241de44cc0c6d12c69898
tgodzik <tgodzik@virtuslab.com>
2020-06-15 19:29:26 +0200

    Update metals versions and switch to cs isntall

--- gitpod_command.sh
+++ gitpod_command.sh
@@ -1,12 +1,9 @@
- #!/bin/bash
+#!/bin/bash
 
 METALS_DIR="$GITPOD_REPO_ROOT/.metals"
-BLOOP_VERSION="1.4.0-RC1-162-888454e1"
+APPS_DIR="$METALS_DIR/apps"
 
-export PATH=$PATH:/usr/local/openjdk-8/bin
+mkdir -p $APPS_DIR
 
-source $METALS_DIR/bloop_local/bash/bloop
-alias sbt=$METALS_DIR/sbt/bin/sbt
-alias bloop=$METALS_DIR/bloop_local/bloop
-
-$METALS_DIR/cs launch ch.epfl.scala:bloopgun-core_2.12:$BLOOP_VERSION --cache=$METALS_DIR/coursier  -- about
+export PATH=$PATH:/usr/local/openjdk-8/bin:$APPS_DIR
+bloop about
--- gitpod_init.sh
+++ gitpod_init.sh
@@ -1,16 +1,12 @@
  #!/bin/bash
 
 METALS_DIR="$GITPOD_REPO_ROOT/.metals"
-METALS_VERSION="0.8.3"
-BLOOP_VERSION="1.4.0-RC1-162-888454e1"
+APPS_DIR="$METALS_DIR/apps"
+METALS_VERSION="0.9.0"
 
-export PATH=$PATH:/usr/local/openjdk-8/bin
+mkdir -p $APPS_DIR
 
-curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py > ~/bloop_install.py
-python ~/bloop_install.py --dest $METALS_DIR/bloop_local
-curl -L https://piccolo.link/sbt-1.3.9.tgz > ~/sbt.tar.gz
-mkdir -p $METALS_DIR/sbt
-tar -xvf ~/sbt.tar.gz -C $METALS_DIR
+export PATH=$PATH:/usr/local/openjdk-8/bin:$APPS_DIR
 
 echo "-Dsbt.coursier.home=$METALS_DIR/coursier" >> .jvmopts
 echo "-Dcoursier.cache=$METALS_DIR/coursier" >> .jvmopts
@@ -18,12 +14,15 @@ echo "-sbt-dir $METALS_DIR/sbt" >> .sbtopts
 echo "-sbt-boot $METALS_DIR/sbt/boot" >> .sbtopts
 echo "-ivy $METALS_DIR/.ivy2" >> .sbtopts
 
-curl -Lo $METALS_DIR/cs https://git.io/coursier-cli-linux && chmod +x $METALS_DIR/cs
-$METALS_DIR/cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=$METALS_DIR/coursier 
-$METALS_DIR/cs fetch org.scalameta:scalafmt-cli_2.12:2.4.2 --cache=$METALS_DIR/coursier 
+curl -Lo $APPS_DIR/cs https://git.io/coursier-cli-linux && chmod +x $APPS_DIR/cs
 
-source $METALS_DIR/bloop_local/bash/bloop
-alias sbt=$METALS_DIR/sbt/bin/sbt
-alias bloop=$METALS_DIR/bloop_local/bloop
+cs install --install-dir $APPS_DIR --only-prebuilt=true bloop
+cs install --install-dir $APPS_DIR sbt
+
+cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=$METALS_DIR/coursier 
+cs fetch org.scalameta:scalafmt-cli_2.12:2.4.2 --cache=$METALS_DIR/coursier 
+
 sbt -Dbloop.export-jar-classifiers=sources bloopInstall
-$METALS_DIR/cs launch ch.epfl.scala:bloopgun-core_2.12:$BLOOP_VERSION --cache=$METALS_DIR/coursier  -- compile --cascade root
+bloop compile --cascade root
+
+echo "export PATH=\$PATH:/usr/local/openjdk-8/bin:$APPS_DIR" >> ~/.bashrc
