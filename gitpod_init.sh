43042c85ac9bd01e4e30adf5f0105774d224e9c8
Tomasz Godzik <tgodzik@users.noreply.github.com>
2024-12-19 15:56:13 +0100

    Update gitpod_init.sh

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -2,11 +2,11 @@
 
 METALS_DIR="$GITPOD_REPO_ROOT/.metals"
 APPS_DIR="$METALS_DIR/apps"
-METALS_VERSION="0.9.10"
+METALS_VERSION="1.4.2"
 
 mkdir -p $APPS_DIR
 
-export PATH=$PATH:/usr/local/openjdk-8/bin:$APPS_DIR
+export PATH=$PATH:/usr/local/openjdk-17/bin:$APPS_DIR
 
 echo "-Dsbt.coursier.home=$METALS_DIR/coursier" >> .jvmopts
 echo "-Dcoursier.cache=$METALS_DIR/coursier" >> .jvmopts
@@ -18,8 +18,8 @@ curl -Lo $APPS_DIR/cs https://git.io/coursier-cli-linux && chmod +x $APPS_DIR/cs
 cs install --install-dir $APPS_DIR --only-prebuilt=true bloop
 cs install --install-dir $APPS_DIR sbt
 
-cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=$METALS_DIR/coursier 
-cs fetch org.scalameta:scalafmt-cli_2.12:2.7.5 --cache=$METALS_DIR/coursier 
+cs fetch org.scalameta:metals_2.13:$METALS_VERSION --cache=$METALS_DIR/coursier 
+cs fetch org.scalameta:scalafmt-cli_2.12:3.8.3 --cache=$METALS_DIR/coursier 
 
 sbt -Dbloop.export-jar-classifiers=sources bloopInstall
 bloop compile --cascade root
